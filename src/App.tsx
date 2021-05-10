7a217686c4d912c5ea9417138f39529f9f3384d8
TINGCYGF <1127163160@qq.com>
2021-05-10 14:56:19 +0800

    回退版本解决svg bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
+import styled from "styled-components";
 import {
   HashRouter as Router,
   Switch,
   Route,
   Redirect
 } from 'react-router-dom';
-import Nav from "./components/Nav";
-import styled from "styled-components";
+import Home from 'views/Home';
+import Label from './views/Label';
+import Detail from 'views/Detail';
+import Account from 'views/Account';
+import Statistics from 'views/Statistics';
+import NotFound from 'views/NotFound';
+
+
 
 const Wrapper = styled.div`
   height:100vh;
   display: flex;
   flex-direction: column;
   background: #eee;
-`;
+`
+
+
 
 function App() {
   return (
     <Router>
       <Wrapper>
         <Switch>
-          <Route path="/home">home</Route>
-          <Route path="/detail">Detail</Route>
-          <Route path="/account">Account</Route>
-          <Route path="/label">Label</Route>
-          <Route path="/statistics">statistics</Route>
+          <Route path="/home"><Home/></Route>
+          <Route path="/detail"><Detail/></Route>
+          <Route path="/account"><Account/></Route>
+          <Route path="/label"><Label/></Route>
+          <Route path="/statistics"><Statistics/></Route>
           <Redirect exact from="/" to="/home"/>
-          <Route path="*">NotFound</Route>
+          <Route path="*"><NotFound/></Route>
         </Switch>
       </Wrapper>
     </Router>

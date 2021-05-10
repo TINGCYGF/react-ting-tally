df894e229a88656d2dd0ef1e5d108b1068b78498
TINGCYGF <1127163160@qq.com>
2021-05-10 13:37:59 +0800

    require目录引入配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import Nav from "./components/Nav";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height:100vh;
+  display: flex;
+  flex-direction: column;
+  background: #eee;
+`;
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Router>
+      <Wrapper>
+        <Switch>
+          <Route path="/home">home</Route>
+          <Route path="/detail">Detail</Route>
+          <Route path="/account">Account</Route>
+          <Route path="/label">Label</Route>
+          <Route path="/statistics">statistics</Route>
+          <Redirect exact from="/" to="/home"/>
+          <Route path="*">NotFound</Route>
+        </Switch>
+      </Wrapper>
+    </Router>
   );
 }
 

1821b6d81f3b2974acb28fc277398a06137a3a3c
TINGCYGF <1127163160@qq.com>
2021-05-10 14:56:01 +0800

    creat 5 views

---
+++ src/views/Account.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/Layout";
+
+
+const Account = ()=>{
+  return (
+    <Layout name="Account">
+      <div>
+        Account
+      </div>
+    </Layout>
+  )
+};
+
+export default Account

285796040f0f7d26becf418fe53d09a1822e4319
TINGCYGF <1127163160@qq.com>
2021-05-12 11:58:27 +0800

    修复id bug，id不重复

--- src/views/Bus/lib-create-id.tsx
+++ src/views/Bus/lib-create-id.tsx
@@ -1,7 +1,8 @@
 let id: number;
-id = 0;
+id = parseInt(window.localStorage.getItem('inMax') || '0')
 const createId = () => {
   id += 1;
+  window.localStorage.setItem('idMax', JSON.stringify(id))
   return id
 }
 
